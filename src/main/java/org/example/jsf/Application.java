fff975e95eaf3210bf3cf60c7abc2af25884db2d
Bauke Scholtz <balusc@gmail.com>
2017-12-09 22:40:34 +0100

    Source code for chapter "Facelets Templating"

---
+++ src/main/java/org/example/jsf/Application.java
@@ -0,0 +1,10 @@
+package org.example.jsf;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.faces.annotation.FacesConfig;
+
+@FacesConfig
+@ApplicationScoped
+public class Application {
+
+}
---
+++ src/main/java/org/example/jsf/Spa.java
@@ -0,0 +1,35 @@
+package org.example.jsf;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.annotation.ManagedProperty;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class Spa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject @ManagedProperty("#{param.page}")
+	private String page;
+
+	@PostConstruct
+	public void init() {
+		if (page == null) {
+			page = "page1";
+		}
+	}
+
+	public void set(String page) {
+		this.page = page;
+	}
+
+	public String getPage() {
+		return page;
+	}
+
+}
